0ce4ec956b09f31a812296b34f3814a02f01a81a
ba-p <phuongtr3112@gmail.com>
2021-01-18 13:36:43 +0700

    create

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const port = 3000;
+
+//mongoose.connect(process.env.MONGO_URL);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log("App listening on port", port);
+});
