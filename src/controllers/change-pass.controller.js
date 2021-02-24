7f11f7566f2aeea8a8b404a1e8f05578f615c46e
ba-p <phuongtr3112@gmail.com>
2021-02-24 22:55:34 +0700

    Fix bug

--- src/controllers/change-pass.controller.js
+++ src/controllers/change-pass.controller.js
@@ -3,21 +3,21 @@ const bcrypt = require("bcrypt");
 
 module.exports.index = async (req, res) => {
   const password = req.body.password;
-  const id = req.body.id;
+  const username = req.body.username;
   const result = {
     errorCode : 0,
     error: ''
   };
-  let user = await User.findOne({ _id: id });
+  let user = await User.findOne({ username: username });
   if (!user){
     result.errorCode = 1;
     result.error = 'User not exist';
-    res.json('hi');
+    res.json(result);
   }
   const hash = await bcrypt.hash(
     password,
     parseInt(process.env.SALT_ROUNDS_BCRYPT)
   );
-  await User.findByIdAndUpdate(id, { password: hash });
-  res.json({ errorCode: 1 });
+  await User.findByIdAndUpdate(user._id, { password: hash });
+  res.json(result);
 };
