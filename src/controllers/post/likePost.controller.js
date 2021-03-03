dc35fead0e76b87405df858199f2a3acf02894a3
ba-p <phuongtr3112@gmail.com>
2021-03-03 21:01:55 +0700

    Done like

---
+++ src/controllers/post/likePost.controller.js
@@ -0,0 +1,19 @@
+const Post = require("../../models/post.model");
+const User = require("../../models/user.model");
+
+module.exports.index = async (req, res) => {
+    const userId = req.body.userId;
+    const postId = req.body.postId;
+
+    const post = await Post.findById(postId);
+    
+    const likes = post.likes;
+    const check = likes.indexOf(userId);
+    if (check === -1){
+        likes.push(userId);
+    } else {
+        likes.splice(check,1);
+    }
+    await Post.findByIdAndUpdate(postId, { likes: likes });
+    res.json('ok');
+} 
--- src/routes/getPost.route.js
+++ src/routes/getPost.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const getPostController = require("../controllers/post/getPost.controller");
+const likePostController = require("../controllers/post/likePost.controller");
 
 router.get("/", getPostController.index);
+
+router.post("/like", likePostController.index);
 
 module.exports = router;
