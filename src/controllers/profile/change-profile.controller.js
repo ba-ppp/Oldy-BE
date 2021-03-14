5800429c5813e9304e8b71396d08d0e76d5c1063
ba-p <phuongtr3112@gmail.com>
2021-03-14 16:06:47 +0700

    Check account

--- src/controllers/profile/change-profile.controller.js
+++ src/controllers/profile/change-profile.controller.js
@@ -1,11 +1,20 @@
 const User = require("../../models/user.model");
 
 module.exports.index = async (req, res) => {
-    const { id, name, username, email } = req.body;
+    const { id, name, email } = req.body;
+    const check = await User.findOne({ email: email });
+    if(check){
+        res.json({
+            errorCode : 1,
+            message: 'Email was used for another account'
+        })
+    }
     const user = await User.findById(id);
     user.name = name;
-    user.username = username;
     user.email = email;
     await User.findByIdAndUpdate(id, user);
-    res.json('done');
+    res.json({
+        errorCode: 0,
+        message: 'done'
+    });
 }
