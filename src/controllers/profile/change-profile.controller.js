03479f6a8c154568068ebff76429c5f75b845c61
ba-p <phuongtr3112@gmail.com>
2021-03-14 15:49:41 +0700

    Done

--- src/controllers/profile/change-profile.controller.js
+++ src/controllers/profile/change-profile.controller.js
@@ -3,12 +3,9 @@ const User = require("../../models/user.model");
 module.exports.index = async (req, res) => {
     const { id, name, username, email } = req.body;
     const user = await User.findById(id);
-    console.log(name);
     user.name = name;
     user.username = username;
     user.email = email;
     await User.findByIdAndUpdate(id, user);
-    const newUser = await User.findById(id);
-    console.log(newUser);
     res.json('done');
 }
