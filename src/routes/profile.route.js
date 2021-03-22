bcf5d2f38974b472b73a0493fc5a517447300b7f
ba-p <phuongtr3112@gmail.com>
2021-03-22 11:13:32 +0700

    File

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const changeProfileController = require("../controllers/profile/change-profile.controller");
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/' }).single('avt');
 
 router.get("/change-profile", (req, res) => {
   res.sendStatus(200);
@@ -14,6 +14,19 @@ router.get("/change-avt", (req, res) => {
 
 router.post("/change-profile", changeProfileController.index);
 
-router.post("/change-avt", changeProfileController.changeAvt);
+router.post("/change-avt", function(req, res) {
+     
+  upload(req, res, function (err) {
+         if (err instanceof multer.MulterError) {
+             return res.status(500).json(err)
+         } else if (err) {
+             return res.status(500).json(err)
+         }
+    return res.json({
+      file: req.file
+    })
+
+  })
+});
 
 module.exports = router;
