e8422b8e46b5a544682d51a2729d90bbf9f102e5
bap <phuongtr3112@gmail.com>
2021-06-03 07:34:06 -0700

    Hell

--- src/controllers/post/upPost.controller.js
+++ src/controllers/post/upPost.controller.js
@@ -2,6 +2,11 @@ const Post = require("../../models/post.model");
 
 module.exports.index = async (req, res) => {
     const { caption, images, userId } = req.body;
+    if(!caption || !images || !userId)
+        res.json({
+            errorCode: 1,
+            message: 'Đăng bài không thành công'
+        })
     const post = {
         userId: userId,
         caption: caption,
